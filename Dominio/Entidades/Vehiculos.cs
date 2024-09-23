4b7fb605ea755381d12e792daf568b4587f38269
Fraylian Alejandro Soto Garcia <20220225@itla.edu.do>
2024-09-23 18:01:33 -0400

    Agregar archivos de proyecto.

---
+++ Dominio/Entidades/Vehiculos.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dominio.Entidades
+{
+    public class Vehiculos
+    {
+        
+        public int id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "el maximo de caracteres ed de 10")]
+        public string Matricula { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Marca { get; set; }
+
+        
+        [Required]
+        [StringLength(50)]
+        public string Modelo { get; set; }
+        [Required]
+        [StringLength(15, ErrorMessage = "El maximo de caracteres es de 15 ")]
+        public string transmision { get; set; }
+
+        public int year { get; set; }
+
+        [Required]
+        [Range(1,10, ErrorMessage = "El maximo de puertas es de 10")]
+        public int numero_Puertas { get; set; }
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "El maximo de asientos es de 10")]
+        public int numero_asientos { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal costo_por_dia { get; set; }
+        [Required]
+        public bool rentado { get; set; }
+        public string descripcion { get; set; }
+        public string? imagen { get; set; }
+
+
+    }
+}
---
+++ Persistencia/Context/ProyectoContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Dominio.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Persistencia.Context
+{
+    public class ProyectoContext: IdentityDbContext<Usuarios>
+    {
+        public ProyectoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Clientes> clientes { get; set; }
+        public DbSet<factura> factura { get; set; }
+        public DbSet<factura_detalle> factura_Detalles { get; set; }
+        public DbSet<Usuarios> usuarios { get; set; }
+        public DbSet<Vehiculos> vehiculos { get; set; }
+    }
+}
