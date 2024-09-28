afcd5538dccc7f590971506ba71ba1df1945c8b2
LuisLachapel <luislachapelcentropeguero@gmail.com>
2024-09-28 12:08:06 -0400

    Creacion de controller y metodos get y post de vehiculo

---
+++ Aplicacion/Vehiculo/listado.cs
@@ -0,0 +1,26 @@
+﻿using Dominio.Entidades;
+using Persistencia.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplicacion.Vehiculo
+{
+    public class listado
+    {
+        public class ListaVehiculos: IRequest<List<Vehiculos>> { }
+        public class Manejador : IRequestHandler<ListaVehiculos, List<Vehiculos>>
+        {
+            private readonly ProyectoContext _context;
+            public Manejador(ProyectoContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Vehiculos>> Handle(ListaVehiculos request, CancellationToken cancellationToken)
+            {
+                var vehiculos = await _context.vehiculos.ToListAsync();
+                return vehiculos;
+            }
+        }
+    }
+}
