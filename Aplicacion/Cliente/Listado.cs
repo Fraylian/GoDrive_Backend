9b3f68750a8960a744252c72196cc5e449c8ae35
LuisLachapel <luislachapelcentropeguero@gmail.com>
2024-09-30 11:24:29 -0400

    Creacion de controller y metodos registrar y listar cliente

---
+++ Aplicacion/Cliente/Listado.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Persistencia.Context;
+using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplicacion.Cliente
+{
+    public class Listado
+    {
+        public class ListaClientes: IRequest<List<Clientes>> { }
+
+        public class Manejador : IRequestHandler<ListaClientes, List<Clientes>>
+        {
+            private readonly ProyectoContext _context;
+            public Manejador(ProyectoContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Clientes>> Handle(ListaClientes request, CancellationToken cancellationToken)
+            {
+                var clientes = await _context.clientes.ToListAsync();
+                return clientes;
+            }
+        }
+    }
+}
---
+++ GoDrive.Api/Controllers/ClienteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Aplicacion.Cliente;
+using MediatR;
+using Dominio.Entidades;
+
+namespace GoDrive.Api.Controllers
+{
+    public class ClienteController : GeneralController
+    {
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Registrar(Registrar.Modelo datos)
+        {
+            return await Mediator.Send(datos);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Clientes>>> Lista()
+        {
+            return await Mediator.Send(new Listado.ListaClientes());
+        }
+
+    }
+}
