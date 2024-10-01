020f0bbdba1ac5c9837e82bc8ec15ee29f147f1e
LuisLachapel <luislachapelcentropeguero@gmail.com>
2024-10-01 16:24:46 -0400

    Creacion del controller y metodo login de usuario

---
+++ GoDrive.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Aplicacion.Metodos.Usuario;
+using MediatR;
+using Dominio.Entidades;
+using Aplicacion.Seguridad;
+
+namespace GoDrive.Api.Controllers
+{
+    public class UsuarioController : GeneralController
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<UsuarioData>> Login(Login.Modelo datos)
+        {
+            return await Mediator.Send(datos);
+        }
+    }
+}
--- GoDrive.Api/Program.cs
+++ GoDrive.Api/Program.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Identity;
 using MediatR;
 using Aplicacion.Vehiculo;
 using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Authentication;
+using Aplicacion.Seguridad;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +17,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v1" });
+    c.CustomSchemaIds(x => x.FullName);
+});
 builder.Services.AddDbContext<ProyectoContext>(options =>
 {
     options.UseSqlServer("name=DefaultConnection");
@@ -22,12 +30,28 @@ builder.Services.AddDbContext<ProyectoContext>(options =>
 builder.Services.AddMediatR(typeof(listado.Manejador).Assembly);
 builder.Services.AddControllers().AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Insertar>());
 
-//var UserBuilder = builder.Services.AddIdentityCore<Usuarios>();
-//var identityBuilder = new IdentityBuilder(UserBuilder.UserType, UserBuilder.Services);
-//identityBuilder.AddEntityFrameworkStores<ProyectoContext>();
-//identityBuilder.AddSignInManager<SignInManager<Usuarios>>();
+var UserBuilder = builder.Services.AddIdentityCore<Usuarios>();
+var identityBuilder = new IdentityBuilder(UserBuilder.UserType, UserBuilder.Services);
+identityBuilder.AddEntityFrameworkStores<ProyectoContext>();
+identityBuilder.AddSignInManager<SignInManager<Usuarios>>();
+builder.Services.TryAddSingleton<ISystemClock, SystemClock>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.User.RequireUniqueEmail = true; 
+});
+
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ProyectoContext>();
+    var userManager = services.GetRequiredService<UserManager<Usuarios>>();
+    var dataPrueba = new DataPrueba();
+    await dataPrueba.InsertarUsuario(context, userManager);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -37,6 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//app.UseAuthentication();
 
 app.UseAuthorization();
 
